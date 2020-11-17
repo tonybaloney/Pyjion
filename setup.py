7462c9ff72cccd589d66386623ebbb3e60823f65
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-11-17 17:53:40 +1100

    Add code coverage to the CI/CD process

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyjion',
-    version='0.0.1',
+    version='0.0.2',
     description='A JIT compiler wrapper for CPython',
     author='Anthony Shaw and Microsoft',
     author_email='anthonyshaw@apache.org',
