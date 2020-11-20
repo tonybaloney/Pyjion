af1d6eef0ecf573a2430cfdbe9c3dc83458f170a
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-11-20 12:24:21 +1100

    Change the build steps

--- .github/build-manylinux.sh
+++ .github/build-manylinux.sh
@@ -2,11 +2,11 @@
 set -e -x
 
 cd /github/workspace/
-alias python3.9=/opt/python/cp39-cp39/bin/python
-python3.9 -m pip install --upgrade --no-cache-dir pip
-python3.9 -m pip install auditwheel
-python3.9 -m pip install .
-python3.9 -m pip wheel . -w ./dist --no-deps
+
+/opt/python/cp39-cp39/bin/pip install --upgrade --no-cache-dir pip
+/opt/python/cp39-cp39/bin/pip install auditwheel
+/opt/python/cp39-cp39/bin/pip install .
+/opt/python/cp39-cp39/bin/pip wheel . -w ./dist --no-deps
 find . -type f -iname "*-linux*.whl" -execdir sh -c "auditwheel repair '{}' -w ./ --plat 'manylinux2014_x86_64' || { echo 'Repairing wheels failed.'; auditwheel show '{}'; exit 1; }" \;
 echo "Succesfully built wheels:"
 find . -type f -iname "*-manylinux*.whl"
