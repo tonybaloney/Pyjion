f71c0124a21a347db8831c2f57d18e93abe29e4f
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-12-10 17:57:02 +1100

    See if I can reproduce the issue on CI

--- src/pyjion/pycomp.cpp
+++ src/pyjion/pycomp.cpp
@@ -858,7 +858,7 @@ bool PythonCompiler::emit_method_call(size_t argCnt) {
         case 3: m_il.emit_call(METHOD_METHCALL_3_TOKEN); return true;
         case 4: m_il.emit_call(METHOD_METHCALL_4_TOKEN); return true;
         case 5: m_il.emit_call(METHOD_METHCALL_5_TOKEN); return true;
-//        case 6: m_il.emit_call(METHOD_METHCALL_6_TOKEN); return true;
+        case 6: m_il.emit_call(METHOD_METHCALL_6_TOKEN); return true;
         case 7: m_il.emit_call(METHOD_METHCALL_7_TOKEN); return true;
         case 8: m_il.emit_call(METHOD_METHCALL_8_TOKEN); return true;
         case 9: m_il.emit_call(METHOD_METHCALL_9_TOKEN); return true;
