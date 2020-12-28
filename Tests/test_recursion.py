746af084eded692d8d64d5e58c31036e38d737ad
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-12-28 05:33:25 +0000

    Remove test that cannot pass on Windows

--- Tests/test_recursion.py
+++ Tests/test_recursion.py
@@ -23,19 +23,6 @@ class RecursionTestCase(unittest.TestCase):
         info = pyjion.info(_f)
         self.assertTrue(info['compiled'])
 
-    def test_overflow(self):
-        def _f():
-            def add_a(z):
-                if len(z) < 2000:
-                    z.append('a')
-                    return add_a(z)
-                return z
-            return add_a([])
-
-        with self.assertRaises(RecursionError):
-            _f()
-        info = pyjion.info(_f)
-        self.assertTrue(info['compiled'])
 
 if __name__ == "__main__":
     unittest.main()
