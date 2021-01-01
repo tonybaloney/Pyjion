d15e42e1b25f05adcd463729db54c767e2ec1df0
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-01-01 12:53:03 +1100

    Start creating tests for basic functionality.

---
+++ Tests/test_lists.py
@@ -0,0 +1,30 @@
+import pyjion
+import unittest
+
+
+
+class ListTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pyjion.enable()
+
+    def tearDown(self) -> None:
+        pyjion.disable()
+
+    def test_list_init(self):
+        l = []
+        l.append(0)
+        self.assertEqual(l, [0])
+
+    def test_list_prepopulated(self):
+        l = [0, 1, 2, 3, 4]
+        l.append(5)
+        self.assertEqual(l, [0, 1, 2, 3, 4, 5])
+
+    def test_list_slice(self):
+        l = [0, 1, 2, 3, 4]
+        self.assertEqual(l[1:2], [1, 2])
+
+
+if __name__ == "__main__":
+    unittest.main()
