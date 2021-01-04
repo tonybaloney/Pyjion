ac145ed1bf4f0b98d81ce74e2e2909d1d14751ee
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-01-04 19:21:57 +1100

    Rewrite the test runner to enable JIT before each test suite and disable after, then collecting GC. Making it far easier to diagnose issues

--- Tests/runlibregr.py
+++ Tests/runlibregr.py
@@ -1,9 +1,32 @@
+import sys
+sys.path.append("/Users/anthonyshaw/CLionProjects/pyjion/src")
+
 import pyjion
+import re
+import os
 import gc
-pyjion.enable()
-from test.libregrtest import main
-main()
-gc.collect()
-pyjion.disable()
-print("Disabling JIT")
-print(pyjion.stats())
+import test.libregrtest.cmdline
+import unittest
+
+args = test.libregrtest.cmdline._parse_args(sys.argv[1:])
+
+SAVEDCWD = os.getcwd()
+tests = []
+
+# Lifted from libregr.main
+regex = re.compile(r'\btest_[a-zA-Z0-9_]+\b')
+with open(os.path.join(SAVEDCWD, args.fromfile)) as fp:
+    for line in fp:
+        line = line.split('#', 1)[0]
+        line = line.strip()
+        match = regex.search(line)
+        if match is not None:
+            tests.append(match.group())
+
+for test in tests:
+    test_cases = unittest.defaultTestLoader.loadTestsFromName(f"test.{test}")
+    pyjion.enable()
+    result = test_cases.run(unittest.result.TestResult())
+    print(result)
+    pyjion.disable()
+    gc.collect()
