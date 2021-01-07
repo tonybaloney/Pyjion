c86a027e8f2d3c5ce0e8f90c5ed66a107e3bc800
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-01-07 12:53:33 +1100

    Add integration tests for tuple ref leaks

--- Tests/test_tuples.py
+++ Tests/test_tuples.py
@@ -1,6 +1,8 @@
 import unittest
 import pyjion
 import gc
+import sys
+
 
 class TupleTestCase(unittest.TestCase):
 
@@ -13,29 +15,41 @@ class TupleTestCase(unittest.TestCase):
 
     def test_tuple_prepopulated(self):
         l = (0, 1, 2, 3, 4)
+        self.assertEqual(sys.getrefcount(l), 5)
         self.assertEqual(l, (0, 1, 2, 3, 4))
+        self.assertEqual(sys.getrefcount(l), 5)
         for i in range(0, 5):
             self.assertEqual(l[i], i)
+        self.assertEqual(sys.getrefcount(l), 5)
         self.assertEqual(l[4], 4)
+        self.assertEqual(sys.getrefcount(l), 5)
 
     def test_tuple_slice(self):
         l = (0, 1, 2, 3, 4)
+        self.assertEqual(sys.getrefcount(l), 5)
         self.assertEqual(l[1:3], (1, 2))
+        self.assertEqual(sys.getrefcount(l), 5)
 
     def test_tuple_unpack(self):
         l = (0, 1, 2, 3, 4)
+        self.assertEqual(sys.getrefcount(l), 5)
         first, second, *_, last = l
+        self.assertEqual(sys.getrefcount(l), 5)
         self.assertEqual(first, 0)
         self.assertEqual(second, 1)
         self.assertEqual(last, 4)
+        self.assertEqual(sys.getrefcount(l), 5)
 
     def test_unknown_type(self):
         def get_vals():
             return (0, 1, 2, 3)
         res = get_vals()
+        self.assertEqual(sys.getrefcount(res), 3)
         self.assertEqual(res[0], 0)
         self.assertEqual(res[1], 1)
         self.assertEqual(res[2], 2)
+        self.assertEqual(sys.getrefcount(res), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
