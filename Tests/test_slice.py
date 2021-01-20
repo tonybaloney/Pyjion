cb3ad0d70dfd5e76426dc388f8bc71396a4273c9
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-01-20 20:11:37 +1100

    Document opt-11

--- Tests/test_slice.py
+++ Tests/test_slice.py
@@ -3,6 +3,8 @@ import pyjion.dis
 import unittest
 import gc
 import sys
+import io
+import contextlib
 
 
 class SliceTestCase(unittest.TestCase):
@@ -75,13 +77,13 @@ class SliceTestCase(unittest.TestCase):
         self.assertEqual(l[x + 1:0], [])
         self.assertEqual(initial_ref, sys.getrefcount(l))
         self.assertEqual(initial_ref_x, sys.getrefcount(x))
-        self.assertEqual(l[:x-1], [0])
+        self.assertEqual(l[:x - 1], [0])
         self.assertEqual(initial_ref, sys.getrefcount(l))
         self.assertEqual(initial_ref_x, sys.getrefcount(x))
-        self.assertEqual(l[x:x+1], [2])
+        self.assertEqual(l[x:x + 1], [2])
         self.assertEqual(initial_ref, sys.getrefcount(l))
         self.assertEqual(initial_ref_x, sys.getrefcount(x))
-        self.assertEqual(l[:x-4], [0, 1])
+        self.assertEqual(l[:x - 4], [0, 1])
         self.assertEqual(initial_ref, sys.getrefcount(l))
         self.assertEqual(initial_ref_x, sys.getrefcount(x))
         self.assertEqual(l[x::-1], [2, 1, 0])
@@ -91,6 +93,85 @@ class SliceTestCase(unittest.TestCase):
 
     def test_string_slicing(self):
         self.assertEqual('The train to Oxford leaves at 3pm'[-1:3:-2], 'm3t ealdox tnat')
+
+    def assertOptimizedSlice(self, func) -> None:
+        self.assertTrue(pyjion.info(func)['compiled'])
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            pyjion.dis.dis(func)
+        self.assertIn("MethodTokens.METHOD_SUBSCR_LIST_SLICE", f.getvalue())
+
+    def test_slice_optimizations(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[0:1]
+
+        self.assertEqual(f(), [0])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations2(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[2:]
+
+        self.assertEqual(f(), [2, 3])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations3(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[:1]
+
+        self.assertEqual(f(), [0])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations4(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[:]
+
+        self.assertEqual(f(), [0, 1, 2, 3])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations5(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[-1:]
+
+        self.assertEqual(f(), [3])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations6(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[::1]
+
+        self.assertEqual(f(), [0, 1, 2, 3])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations7(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[::-1]
+
+        self.assertEqual(f(), [3, 2, 1, 0])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations8(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[2::1]
+
+        self.assertEqual(f(), [2, 3])
+        self.assertOptimizedSlice(f)
+
+    def test_slice_optimizations9(self):
+        def f():
+            l = [0, 1, 2, 3]
+            return l[0:1:1]
+
+        self.assertEqual(f(), [0])
+        self.assertOptimizedSlice(f)
 
 
 if __name__ == "__main__":
