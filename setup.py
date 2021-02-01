7b16c017802a5ca3dcc55d594544ba96abb7521e
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-02-01 17:07:37 +1100

    Update version numbers

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("CHANGELOG.md", "r") as fh:
 
 setup(
     name='pyjion',
-    version='0.9.0',
+    version='0.10.0',
     description='A JIT compiler wrapper for CPython',
     author='Anthony Shaw and Microsoft',
     author_email='anthonyshaw@apache.org',
