bc12dbe708f21ac31a6588ef905c0fa76c833fe8
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-03-23 18:52:09 +1100

    Enable PGC for BINARY_SUBSCR

--- src/pyjion/absint.cpp
+++ src/pyjion/absint.cpp
@@ -374,7 +374,20 @@ AbstractInterpreter::interpret(PyObject *builtins, PyObject *globals, PyjionCode
                     lastState.getLocal(oparg).ValueInfo.escapes();
                     lastState.replaceLocal(oparg, AbstractLocalInfo(&Undefined, true));
                     break;
-                case BINARY_SUBSCR:
+                case BINARY_SUBSCR:{
+                    AbstractValueWithSources two;
+                    AbstractValueWithSources one;
+                    if (PGC_READY()){
+                        PGC_PROBE(2);
+                        PGC_UPDATE_STACK(2);
+                    }
+                    two = lastState.popNoEscape();
+                    one = lastState.popNoEscape();
+
+                    auto out = one.Value->binary(one.Sources, opcode, two);
+                    lastState.push(out);
+                }
+                break;
                 case BINARY_TRUE_DIVIDE:
                 case BINARY_FLOOR_DIVIDE:
                 case BINARY_POWER:
@@ -405,7 +418,7 @@ AbstractInterpreter::interpret(PyObject *builtins, PyObject *globals, PyjionCode
                     AbstractValueWithSources one;
                     if (PGC_READY()){
                         PGC_PROBE(2);
-                        // PGC_UPDATE_STACK(2);
+                        //PGC_UPDATE_STACK(2);
                     }
                     two = lastState.popNoEscape();
                     one = lastState.popNoEscape();
