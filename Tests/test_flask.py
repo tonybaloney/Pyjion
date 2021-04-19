14fca2be618fcd2e9b414c4379943900e2e96aa8
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-04-19 14:13:51 +1000

    Fix a crash on PGC compilation for freed last observed value

---
+++ Tests/test_flask.py
@@ -0,0 +1,16 @@
+import pyjion
+from pyjion.wsgi import PyjionWsgiMiddleware
+
+from flask import Flask
+app = Flask(__name__)
+
+# Override the app wsgi_app property
+app.wsgi_app = PyjionWsgiMiddleware(app.wsgi_app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Pycon BY!'
+
+if __name__ == "__main__":
+    pyjion.enable()
+    app.run()
