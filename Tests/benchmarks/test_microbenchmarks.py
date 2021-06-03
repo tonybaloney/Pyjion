52b25d5e793acd168452c38423cbdc51040015fe
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-03 15:04:08 +1000

    Add an int benchmark

--- Tests/benchmarks/test_microbenchmarks.py
+++ Tests/benchmarks/test_microbenchmarks.py
@@ -1,15 +1,23 @@
 import pyjion
 import timeit
 
+
 def test_floats(n=10000):
-    for y in range(10000):
+    for y in range(n):
         x = 0.1
         z = y * y + x - y
         x *= z
 
 
+def test_ints(n=10000):
+    for y in range(n):
+        x = 2
+        z = y * y + x - y
+        x *= z
+
+
 if __name__ == "__main__":
-    tests = (test_floats, )
+    tests = (test_floats, test_ints)
     for test in tests:
         print("{1} took {0} without Pyjion".format(timeit.repeat(test, repeat=5, number=1000), str(test)))
         pyjion.enable()
