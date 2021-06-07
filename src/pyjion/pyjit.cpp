1502b233397796fd66588bfcf7d05b469f701a16
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-08 08:43:55 +1000

    Make sequence point dumps a toggle

--- src/pyjion/pyjit.cpp
+++ src/pyjion/pyjit.cpp
@@ -242,7 +242,7 @@ PyObject* PyJit_ExecuteAndCompileFrame(PyjionJittedCode* state, PyFrameObject *f
     state->j_sequencePoints = res.compiledCode->get_sequence_points();
     state->j_sequencePointsLen = res.compiledCode->get_sequence_points_length();
 
-#ifdef DUMP_JIT_TRACES
+#ifdef DUMP_SEQUENCE_POINTS
     printf("Method disassembly for %s\n", PyUnicode_AsUTF8(frame->f_code->co_name));
     auto code = (_Py_CODEUNIT *)PyBytes_AS_STRING(frame->f_code->co_code);
     for (size_t i = 0; i < state->j_sequencePointsLen; i ++){
