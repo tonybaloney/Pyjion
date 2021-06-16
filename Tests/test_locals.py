68fb16b9f32059691c465b1cf40cea0b5c543f4d
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-16 14:33:51 +1000

    Update the locals tests

--- Tests/test_locals.py
+++ Tests/test_locals.py
@@ -1,13 +1,11 @@
 """Test the optimization of frame locals"""
 import unittest
 import pyjion
-import io
 import pyjion.dis
-import contextlib
 import gc
 
-class LocalsTestCase(unittest.TestCase):
 
+class LocalsTestCase(unittest.TestCase):
     def setUp(self) -> None:
         pyjion.enable()
 
@@ -15,20 +13,13 @@ class LocalsTestCase(unittest.TestCase):
         pyjion.disable()
         gc.collect()
 
-    def assertOptimized(self, func) -> None:
-        self.assertFalse(func())
-        self.assertTrue(pyjion.info(func)['compiled'])
-        f = io.StringIO()
-        with contextlib.redirect_stdout(f):
-            pyjion.dis.dis(func)
-        self.assertIn("ldloc.s", f.getvalue())
-
     def test_simple_compare(self):
         def test_f():
             a = 1
             b = 2
             return a == b
-        self.assertOptimized(test_f)
+
+        self.assertTrue(test_f())
 
     def test_simple_delete(self):
         def test_f():
@@ -37,7 +28,8 @@ class LocalsTestCase(unittest.TestCase):
             del a
             del b
             return False
-        self.assertOptimized(test_f)
+
+        self.assertFalse(test_f())
 
 
 if __name__ == "__main__":
