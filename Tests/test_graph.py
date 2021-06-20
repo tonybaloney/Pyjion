469b288db7ccac879b1e29cb411438d15aa8dc62
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-20 16:14:14 +1000

    Add an API to export graphs

---
+++ Tests/test_graph.py
@@ -0,0 +1,25 @@
+import pyjion
+import unittest
+import gc
+
+
+class GraphTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pyjion.enable()
+        pyjion.enable_graphs()
+
+    def tearDown(self) -> None:
+        pyjion.disable()
+        pyjion.disable_graphs()
+        gc.collect()
+
+    def test_graph_dump(self) -> None:
+        def f():
+            a = 1
+            b = 2
+            return a + b
+        f()
+        self.assertNotNone(pyjion.get_graph(f))
+        f()
+        self.assertNotNone(pyjion.get_graph(f))
