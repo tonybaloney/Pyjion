32ee6e742b83993a5cd0b46930c04688df157958
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-12-07 09:36:25 +1100

    rewrite arch to meet the dotnet names

--- eng/install-dotnet.sh
+++ eng/install-dotnet.sh
@@ -1,5 +1,8 @@
 echo "Building Pyjion with .NET  $DOTNET_VERSION"
 ARCH=`uname -m`
+if [$ARCH = "x86_64"]; then
+    ARCH="x64"
+fi
 yum install -y wget && yum clean all
 wget -q https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-${ARCH}.tar.gz
 mkdir -p /tmp/dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-${ARCH}.tar.gz -C /tmp/dotnet
