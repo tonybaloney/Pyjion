69cecdf8b54d94b86141e9c57ee5c4e3957dced2
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-12-07 09:30:50 +1100

    Install CPU arch specific dotnet

--- eng/install-dotnet.sh
+++ eng/install-dotnet.sh
@@ -1,4 +1,5 @@
 echo "Building Pyjion with .NET  $DOTNET_VERSION"
+ARCH=`uname -m`
 yum install -y wget && yum clean all
-wget -q https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz
-mkdir -p /tmp/dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz -C /tmp/dotnet
+wget -q https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-${ARCH}.tar.gz
+mkdir -p /tmp/dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-${ARCH}.tar.gz -C /tmp/dotnet
