ad908ed6ff4f3e7fdc37e500d01e86300fa1bef6
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-12-09 14:08:59 +1100

    Add sqlalchemy tests to better understand reported bug

---
+++ Tests/thirdparty/test_sqlalchemy.py
@@ -0,0 +1,33 @@
+import pytest
+try:
+    from sqlalchemy import Column, select, Integer, String, create_engine
+    from sqlalchemy.orm import declarative_base, Session
+
+    has_lib = True
+except ImportError:
+    has_lib = False
+
+
+@pytest.mark.skipif(not has_lib, reason="Missing library")
+@pytest.mark.external
+def test_base_type():
+    # declarative base class
+    Base = declarative_base()
+    engine = create_engine('sqlite://')
+
+    class Post(Base):
+        __tablename__ = 'post'
+        id = Column(Integer, autoincrement=True, primary_key=True)
+        name = Column(String)
+
+    # create session and add objects
+    with Session(engine) as session:
+        Base.metadata.create_all(engine)
+
+        session.add(Post(name='first post'))
+        session.add(Post(name='second post'))
+        session.commit()
+
+        sp = select(Post)
+        ps = session.execute(select(Post)).scalars().all()
+        assert len(ps) == 2
